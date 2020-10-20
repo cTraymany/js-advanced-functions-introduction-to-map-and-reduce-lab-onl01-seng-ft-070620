13569f083645699d1c896cfee1fc98013c21f962
cTraymany <chindalatht@gmail.com>
2020-10-19 22:18:53 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Your code here
+// pry = require('pryjs')
+function mapToNegativize(sourceArray) {
+    let answer = []
+    sourceArray.forEach(index => {
+        if (index < 0) {
+            answer.push(Math.abs(index))
+        } else {
+            answer.push(-Math.abs(index))
+        }
+    })
+    return answer
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray
+}
+
+function mapToDouble(sourceArray) {
+    let answer = []
+    sourceArray.forEach(num => answer.push(num*2))
+    return answer
+}
+
+function mapToSquare(sourceArray) {
+    let answer = []
+    sourceArray.forEach(num => answer.push(num**2))
+    return answer
+}
+
+
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    let answer = 0
+    sourceArray.forEach(num => answer+= num)
+    return answer += startingPoint
+}
+
+function reduceToAllTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++ ) {
+      if (!sourceArray[i]) return false
+    }
+    return true
+  }
+
+function reduceToAnyTrue(sourceArray) {
+    if (sourceArray.find(index => index === true)) {
+        return true
+    } else {
+        return false
+    }
+}
